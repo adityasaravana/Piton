a3f99019898038e6aa2b549c887f15444fad6745
Aditya Saravana <aditya.saravana@icloud.com>
2024-10-07 18:41:41 -0700

    Added a class to store the Screen Time password, and automatically update a corresponding value in the Secure Enclave for persistence

---
+++ Piton/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Piton
+//
+//  Created by Aditya Saravana on 10/7/24.
+//
+
+import Foundation
+
+enum Constants {
+    static let screenTimePasswordSecureEnclaveKey = "com.adityasaravana.Piton.screenTimePassword"
+}
---
+++ Piton/Vault.swift
@@ -0,0 +1,33 @@
+//
+//  Vault.swift
+//  Piton
+//
+//  Created by Aditya Saravana on 10/7/24.
+//
+
+import Foundation
+import Valet
+
+class Vault: ObservableObject {
+    let valet = SecureEnclaveValet.valet(with: Identifier(nonEmpty: "com.adityasaravana.Piton")!, accessControl: .userPresence)
+    
+    @Published var screenTimePassword: String = "" {
+        didSet {
+            print("screenTimePassword changed")
+            do {
+                try valet.setString(screenTimePassword, forKey: Constants.screenTimePasswordSecureEnclaveKey)
+            } catch {
+                fatalError("Could not write Screen Time Password to Secure Enclave. Email aditya.saravana@icloud.com for help.")
+            }
+        }
+    }
+    
+    func readScreenTimePassword() -> String? {
+        do {
+            let password = try valet.string(forKey: Constants.screenTimePasswordSecureEnclaveKey, withPrompt: "")
+            return password
+        } catch {
+            return nil
+        }
+    }
+}
