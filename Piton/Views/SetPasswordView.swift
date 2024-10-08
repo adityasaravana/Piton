c7296a07e1a7391c8b4e4b7313c0a73390ba068a
Aditya Saravana <aditya.saravana@icloud.com>
2024-10-07 18:08:36 -0700

    added a view to set the screen time password

---
+++ Piton/Views/SetPasswordView.swift
@@ -0,0 +1,117 @@
+//
+//  ContentView.swift
+//  Dion
+//
+//  Created by Aditya Saravana on 10/7/24.
+//
+
+import SwiftUI
+import MIPinCodeView
+
+private enum SetPasswordViewStage {
+    case set
+    case confirm
+}
+
+struct SetPasswordView: View {
+    @Binding var password: String
+    @Binding var isPresented: Bool
+    
+    @State private var setPassword: String = ""
+    @State private var confirmPassword: String = ""
+    @State private var stage: SetPasswordViewStage = .set
+    @State private var showPasswordsNotMatchingAlert = false
+    
+    var titleText: String {
+        switch stage {
+        case .set:
+            return "Enter Screen Time password"
+        case .confirm:
+            return "Confirm password"
+        }
+    }
+    
+    var buttonText: String {
+        switch stage {
+        case .set:
+            return "Next"
+        case .confirm:
+            return "Done"
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                
+                Spacer()
+                
+                Text(titleText).font(.headline)
+                
+                switch stage {
+                case .set:
+                    MIPinCodeView(
+                        pin: $setPassword,
+                        length: 4,
+                        isSecure: false,
+                        style: .boxed,
+                        backgroundColor: .gray,
+                        textColor: .white,
+                        cornerRadius: 8,
+                        width: 40,
+                        height: 40,
+                        keyboardType: .numberPad
+                    )
+                case .confirm:
+                    MIPinCodeView(
+                        pin: $confirmPassword,
+                        length: 4,
+                        isSecure: false,
+                        style: .boxed,
+                        backgroundColor: .gray,
+                        textColor: .white,
+                        cornerRadius: 8,
+                        width: 40,
+                        height: 40,
+                        keyboardType: .numberPad
+                    )
+                }
+                
+                Spacer()
+                
+                Button("Next") {
+                    withAnimation {
+                        switch stage {
+                        case .set:
+                            stage = .confirm
+                        case .confirm:
+                            if setPassword != confirmPassword {
+                                showPasswordsNotMatchingAlert = true
+                            } else {
+                                password = setPassword
+                                isPresented = false
+                            }
+                        }
+                    }
+                }
+                .disabled(setPassword.count != 4)
+                .buttonStyle(.borderedProminent)
+                .keyboardShortcut(.defaultAction)
+            }
+            .alert("Your passwords don't match.", isPresented: $showPasswordsNotMatchingAlert) {
+                Button("OK", role: .cancel) { }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Cancel") {
+                        
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SetPasswordView(password: .constant(""), isPresented: .constant(true))
+}
