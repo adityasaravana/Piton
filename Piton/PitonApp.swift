bb8f846d911bba6927ac7bed75e19a50c0251158
Aditya Saravana <aditya.saravana@icloud.com>
2024-10-07 18:00:11 -0700

    initial xcode project

---
+++ Piton/PitonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitonApp.swift
+//  Piton
+//
+//  Created by Aditya Saravana on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PitonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
