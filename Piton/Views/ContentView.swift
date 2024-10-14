e5a651784e190be76caf17fa76e136a80eb30cd8
Aditya Saravana <aditya.saravana@icloud.com>
2024-10-14 15:42:47 -0700

    moved the reset button to the toolbar

--- Piton/Views/ContentView.swift
+++ Piton/Views/ContentView.swift
@@ -13,59 +13,60 @@ struct ContentView: View {
     @State var showRandomTextLockView = false
     
     var body: some View {
-        VStack(spacing: 20) {
-            Spacer()
-            
-            if !vault.screenTimePasswordIsValid {
-                Text("Welcome to Piton")
-                
-                Text("Explanation, information, prompt here")
-                
-                Button("Set Password") {
-                    showSetPasswordSheet = true
-                }
-                .onAppear {
-                    vault.screenTimePassword.removeAll()
-                }
-            } else if vault.screenTimePasswordIsValid && (vault.lockType == LockType.none || {
-                if case .error = vault.lockType {
-                    return true
+        NavigationStack {
+            VStack(spacing: 20) {
+                if !vault.screenTimePasswordIsValid {
+                    Text("Welcome to Piton")
+                    
+                    Text("Explanation, information, prompt here")
+                    
+                    Button("Set Password") {
+                        showSetPasswordSheet = true
+                    }
+                    .onAppear {
+                        vault.screenTimePassword.removeAll()
+                    }
+                } else if vault.screenTimePasswordIsValid && (vault.lockType == LockType.none || {
+                    if case .error = vault.lockType {
+                        return true
+                    } else {
+                        return false
+                    }
+                }()) {
+                    SetLockView()
                 } else {
-                    return false
-                }
-            }()) {
-                SetLockView()
-            } else {
-                Text("Setup complete, this is the home screen now")
-                
-                Text("DEBUG:")
-                Text(LockType.encode(vault.lockType))
-                Text(vault.screenTimePassword)
-                
-                Button("Show Random Text Lock View") {
-                    showRandomTextLockView = true
+                    Text("Setup complete, this is the home screen now")
+                    
+                    Text("DEBUG:")
+                    Text(LockType.encode(vault.lockType))
+                    Text(vault.screenTimePassword)
+                    
+                    Button("Show Random Text Lock View") {
+                        showRandomTextLockView = true
+                    }
                 }
             }
-            
-            Spacer()
-            
-            Button("DEBUG: Reset") {
-                vault.screenTimePassword.removeAll()
-                vault.lockType = .none
+            .padding()
+            .onAppear {
+                print(vault.screenTimePasswordIsValid)
+                print(vault.lockType)
             }
-            .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
-        }
-        .padding()
-        .onAppear {
-            print(vault.screenTimePasswordIsValid)
-            print(vault.lockType)
-        }
-        .fullScreenCover(isPresented: $showSetPasswordSheet) {
-            SetPasswordView(password: $vault.screenTimePassword, isPresented: $showSetPasswordSheet)
-        }
-        .sheet(isPresented: $showRandomTextLockView) {
-            RandomTextLockView(wordCount: 3)
+            .fullScreenCover(isPresented: $showSetPasswordSheet) {
+                SetPasswordView(password: $vault.screenTimePassword, isPresented: $showSetPasswordSheet)
+            }
+            .sheet(isPresented: $showRandomTextLockView) {
+                RandomTextLockView(wordCount: 3)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        vault.screenTimePassword.removeAll()
+                        vault.lockType = .none
+                    } label: {
+                        Image(systemName: "ladybug")
+                    }
+                }
+            }
         }
     }
 }
